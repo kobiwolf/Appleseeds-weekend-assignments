0bfff3388db691ff346a389476b89aab64ac4b2c
kobi wolf <kobiwolf95@gmail.com>
2021-02-25 16:08:23 +0200

    adding style

--- minecraft/js/index.js
+++ minecraft/js/index.js
@@ -1,6 +1,14 @@
 let startBut = document.querySelector('.start');
 let firstPage = document.querySelector('.firstpage');
+let game = document.querySelector('.game');
+let restartBut = document.querySelector('.restartBut');
+function startOver() {
+  console.log('hi');
+  location.reload();
+}
 function startGame() {
-  firstPage.style.opacity = '0';
+  firstPage.style.visibility = 'hidden';
+  game.style.opacity = '1';
 }
 startBut.addEventListener('click', startGame);
+restartBut.addEventListener('click', startOver);
