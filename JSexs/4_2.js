c9c7ee117da188990018ab5916cd98eba3e778ac
kobi wolf <kobiwolf95@gmail.com>
2021-02-25 14:46:25 +0200

    start of minecraft exs

--- JSexs/4_2.js
+++ JSexs/4_2.js
@@ -1,8 +1,13 @@
-function tibo(num) {
-  let arr = [1, 1, 1, ...Array(num - 2)];
-  for (let i = 3; i < arr.length; i++) {
+function tibo(start, num) {
+  if (num === 0) return console.log([]);
+  booli = true;
+  start.forEach((v) => {
+    if (v < 0) booli = false;
+  });
+  if (booli === false) return console.log(false);
+  let arr = [...start, ...Array(num - 2)];
+  for (let i = 3; i < arr.length; i++)
     arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
-  }
   console.log(arr);
 }
-tibo(8);
+tibo([-1, 1, 1], 3);
