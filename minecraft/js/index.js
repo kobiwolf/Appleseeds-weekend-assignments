cb31035d9a9e41aafeb3eefdb0b8b630aace7e64
kobi wolf <kobiwolf95@gmail.com>
2021-02-27 19:55:35 +0200

    almost finish styling

--- minecraft/js/index.js
+++ minecraft/js/index.js
@@ -1,7 +1,10 @@
+// elements
 let startBut = document.querySelector('.start');
 let firstPage = document.querySelector('.firstpage');
 let game = document.querySelector('.game');
 let restartBut = document.querySelector('.restartBut');
+let world = document.querySelector('.world');
+// functions
 function startOver() {
   console.log('hi');
   location.reload();
@@ -10,5 +13,40 @@ function startGame() {
   firstPage.style.visibility = 'hidden';
   game.style.opacity = '1';
 }
+function makeWidth(width) {
+  world.style.width = `${width}px`;
+}
+function makeTree(div) {
+  let row = div.data - row;
+  console.log(row);
+}
+// Events
 startBut.addEventListener('click', startGame);
 restartBut.addEventListener('click', startOver);
+
+function makeMap(num) {
+  let cubeW = Math.floor(world.scrollWidth / num);
+  let cubeH = Math.floor(world.scrollHeight / num);
+  world.style.gridTemplateColumns = `repeat(${cubeW},1fr)`;
+  world.style.gridTemplateRows = `repeat(${cubeH},1fr)`;
+  for (let row = 1; row <= cubeH; row++) {
+    for (let col = 1; col <= cubeW; col++) {
+      let currentRow = cubeH - row;
+      let currentCol = cubeW - col;
+      let div = document.createElement('div');
+      div.dataset.row = `${row}`;
+      div.dataset.col = `${col}`;
+      world.appendChild(div);
+      if (currentRow === 5 && currentCol % 5 === 0) {
+        //    makeTree(div);
+      } else if (currentRow === 4) {
+        div.classList.add('topland');
+      } else if (currentRow < 4) {
+        div.classList.add('dirt');
+      } else {
+        div.classList.add('sky');
+      }
+    }
+  }
+}
+makeMap(30);
