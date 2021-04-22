f55db14cef4956fdf6e04a93bc28e3cbc8f34378
kobi wolf <kobiwolf95@gmail.com>
2021-04-22 18:23:56 +0300

    $npm_config_commit

--- bank-mongodb/server/app.js
+++ bank-mongodb/server/app.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.use(route);
-console.log('wow');
+console.log('wow2');
 app.listen(port, () => {
   console.log(`we are line on ${port}`);
 });
