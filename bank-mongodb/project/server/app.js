a70780f849f92e5ae2609a16786525c0b77e29e7
kobi wolf <kobiwolf95@gmail.com>
2021-04-23 01:51:13 +0300

    a

--- bank-mongodb/project/server/app.js
+++ bank-mongodb/project/server/app.js
@@ -5,16 +5,13 @@ const path = require('path');
 const route = require('./routes/route');
 
 const app = express();
-const pathToClientBuild = path.join(
-  __dirname,
-  './bank-mongodb/project/client/build'
-);
+const pathToClientBuild = path.join(__dirname, '../client/build');
 const port = process.env.PORT || 3001;
 app.use(express.static(pathToClientBuild));
 app.use(express.json());
 app.use(cors());
 app.use(route);
-
+console.log(global.appRoot);
 app.listen(port, () => {
   console.log(`we are line on ${port}`);
 });
