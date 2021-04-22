a67c3c0a3938384e6e0daef7bc1b9570b2280c02
kobi wolf <kobiwolf95@gmail.com>
2021-04-22 19:20:02 +0300

    check

--- bank-mongodb/server/app.js
+++ bank-mongodb/server/app.js
@@ -10,7 +10,6 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 app.use(route);
-//just a check
 app.listen(port, () => {
   console.log(`we are line on ${port}`);
 });
